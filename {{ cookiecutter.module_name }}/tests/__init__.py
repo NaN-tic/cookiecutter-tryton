7fa5baed16d294d846f2a9c3a7c3d5eba6f98dd1
Raimon Esteve <raimon@nan-tic.com>
2018-08-22 15:39:58 +0200

    Initial commit

---
+++ {{ cookiecutter.module_name }}/tests/__init__.py
@@ -0,0 +1,6 @@
+# This file is part {{ cookiecutter.module_name }} module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+from .test_{{ cookiecutter.module_name }} import suite
+
+__all__ = ['suite']
