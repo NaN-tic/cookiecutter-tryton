49b2d4a24d7fd3cf2fb01ade775dea9fd2882f58
Raimon Esteve <raimonesteve@gmail.com>
2020-05-19 09:35:41 +0200

    Upgrade setup and tox and add Jenkinsfile

--- {{ cookiecutter.module_name }}/setup.py
+++ {{ cookiecutter.module_name }}/setup.py
@@ -2,20 +2,21 @@
 # This file is part {{ cookiecutter.module_name }} module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
+# encoding: utf-8
 
 from setuptools import setup
 import re
 import os
 import io
-try:
-    from configparser import ConfigParser
-except ImportError:
-    from ConfigParser import ConfigParser
+from configparser import ConfigParser
 
-{%- if cookiecutter.prefix %}
-
+MODULE = '{{ cookiecutter.module_name }}'
+PREFIX = 'nantic'
 MODULE2PREFIX = {}
-{%- endif %}
+OWNER = {
+    'nantic':'NaN-tic',
+    'trytonzz':'nanticzz',
+}
 
 
 def read(fname):
@@ -33,59 +34,83 @@ def get_require_version(name):
         major_version, minor_version + 1)
     return require
 
+def get_requires(depends='depends'):
+  requires = []
+  for dep in info.get(depends, []):
+      if not re.match(r'(ir|res)(\W|$)', dep):
+          prefix = MODULE2PREFIX.get(dep, 'trytond')
+          owner = OWNER.get(prefix, prefix)
+          if prefix == 'trytond':
+              requires.append(get_require_version('%s_%s' % (prefix, dep)))
+          else:
+              requires.append(
+                  '%(prefix)s-%(dep)s@git+https://github.com/%(owner)s/'
+                  'trytond-%(dep)s.git@%(branch)s'
+                  '#egg=%(prefix)s-%(dep)s-%(series)s'%{
+                          'prefix': prefix,
+                          'owner': owner,
+                          'dep':dep,
+                          'branch': branch,
+                          'series': series,})
+
+  return requires
+
 config = ConfigParser()
 config.readfp(open('tryton.cfg'))
 info = dict(config.items('tryton'))
 for key in ('depends', 'extras_depend', 'xml'):
     if key in info:
         info[key] = info[key].strip().splitlines()
+
 version = info.get('version', '0.0.1')
 major_version, minor_version, _ = version.split('.', 2)
 major_version = int(major_version)
 minor_version = int(minor_version)
-name = '{{ cookiecutter.package_name }}'
-download_url = 'https://bitbucket.org/{{ cookiecutter.download }}/trytond-{{ cookiecutter.module_name }}'
 
 requires = []
-for dep in info.get('depends', []):
-    if not re.match(r'(ir|res)(\W|$)', dep):
-{%- if not cookiecutter.prefix %}
-        requires.append(get_require_version('trytond_%s' % dep))
-{% else %}
-        prefix = MODULE2PREFIX.get(dep, 'trytond')
-        requires.append(get_require_version('%s_%s' % (prefix, dep)))
-{% endif -%}
-requires.append(get_require_version('trytond'))
 
-{% if cookiecutter.test_with_scenario == 'y' -%}
-tests_require = [get_require_version('proteus')]
-{%- else -%}
-tests_require = []
-{%- endif %}
+series = '%s.%s' % (major_version, minor_version)
+if minor_version % 2:
+    branch = 'master'
+else:
+    branch = series
+
+requires += get_requires('depends')
+
+tests_require = [
+    get_require_version('proteus'),
+
+    ]
+tests_require += get_requires('extras_depend')
+# requires += [get_require_version('')]
+
 dependency_links = []
+
 if minor_version % 2:
     # Add development index for testing with proteus
     dependency_links.append('https://trydevpi.tryton.org/')
 
-setup(name=name,
+setup(name='%s_%s' % (PREFIX, MODULE),
     version=version,
-    description='Tryton {{ cookiecutter.description or cookiecutter.module_name.replace('_', ' ').title() }} Module',
+    description='',
     long_description=read('README'),
     author='{{ cookiecutter.author }}',
     author_email='{{ cookiecutter.author_email }}',
     url='{{ cookiecutter.url }}',
-    download_url=download_url,
-    keywords='{{ cookiecutter.keywords }}',
-    package_dir={'trytond.modules.{{ cookiecutter.module_name }}': '.'},
+    download_url='https://github.com:{{ cookiecutter.download }}/trytond-{{ cookiecutter.module_name }}.git',
+    package_dir={'trytond.modules.%s' % MODULE: '.'},
     packages=[
-        'trytond.modules.{{ cookiecutter.module_name }}',
-        'trytond.modules.{{ cookiecutter.module_name }}.tests',
+        'trytond.modules.%s' % MODULE,
+        'trytond.modules.%s.tests' % MODULE,
         ],
     package_data={
-        'trytond.modules.{{ cookiecutter.module_name }}': (info.get('xml', [])
-            + ['tryton.cfg', 'view/*.xml', 'locale/*.po', '*.odt',
-                'icons/*.svg', 'tests/*.rst']),
+        'trytond.modules.%s' % MODULE: (info.get('xml', [])
+            + ['tryton.cfg', 'locale/*.po', 'tests/*.rst', 'view/*.xml',
+            'icons/*.svg']),
         },
+    project_urls = {
+       "Source Code": 'https://github.com:NaN-tic/trytond-{{ cookiecutter.module_name }}.git'
+    },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Plugins',
@@ -98,12 +123,8 @@ setup(name=name,
         'Natural Language :: English',
         'Natural Language :: Spanish',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Office/Business',
         ],
     license='GPL-3',
@@ -112,13 +133,10 @@ setup(name=name,
     zip_safe=False,
     entry_points="""
     [trytond.modules]
-    {{ cookiecutter.module_name }} = trytond.modules.{{ cookiecutter.module_name }}
-    """,
+    %s = trytond.modules.%s
+    """ % (MODULE, MODULE),
     test_suite='tests',
     test_loader='trytond.test_loader:Loader',
     tests_require=tests_require,
-    use_2to3=True,
-{%- if cookiecutter.test_with_scenario == 'y' %}
-    convert_2to3_doctests=['tests/scenario_{{ cookiecutter.module_name }}.rst'],
-{%- endif %}
+
     )
